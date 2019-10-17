93ab4eda12cc49ae481cf19159cc780f1be6069d
Utku Demir <me@utdemir.com>
2019-10-18 11:56:19 +1300

    some work

--- nix/firefox-addons.nix
+++ nix/firefox-addons.nix
@@ -15,10 +15,10 @@
       };
     "privacy-badger17" = buildFirefoxXpiAddon {
       pname = "privacy-badger17";
-      version = "2019.7.1.1";
+      version = "2019.10.8";
       addonId = "jid1-MnnxcxisBPnSXQ@jetpack";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3285452/privacy_badger-2019.7.1.1-an+fx.xpi?src=";
-      sha256 = "7bf4f0a7f527ac3d55f5917b994b2cbe2f4c907d57463f9bd8d04ffa82dc8dad";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3420722/privacy_badger-2019.10.8-an+fx.xpi?src=";
+      sha256 = "14bb91f75b9886d7269e89fa5bfbe1032eb4f42c466ce80fbc61be8fd80bf4df";
       meta = with stdenv.lib;
       {
         homepage = "https://www.eff.org/privacybadger";
@@ -27,30 +27,17 @@
         platforms = platforms.all;
         };
       };
-    "tridactyl-vim" = buildFirefoxXpiAddon {
-      pname = "tridactyl-vim";
-      version = "1.16.2";
-      addonId = "tridactyl.vim@cmcaine.co.uk";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3348489/tridactyl-1.16.2-an+fx.xpi?src=";
-      sha256 = "5aadd08d0c557ae38d37a82534110585fa81e20c5068cfd09cd49be008cd6365";
+    "vim-vixen" = buildFirefoxXpiAddon {
+      pname = "vim-vixen";
+      version = "0.25";
+      addonId = "vim-vixen@i-beam.org";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3421772/vim_vixen-0.25-an+fx.xpi?src=";
+      sha256 = "1cc97e83d7fe90572b6e5344bc73ff022840cd90f029c69f6bf4d2eb5d6a436c";
       meta = with stdenv.lib;
       {
-        homepage = "https://github.com/cmcaine/tridactyl";
-        description = "Vim, but in your browser. Replace Firefox's control mechanism with one modelled on Vim.\n\nThis addon is very usable, but is in an early stage of development. We intend to implement the majority of Vimperator's features.";
-        platforms = platforms.all;
-        };
-      };
-    "ublock-origin" = buildFirefoxXpiAddon {
-      pname = "ublock-origin";
-      version = "1.21.2";
-      addonId = "uBlock0@raymondhill.net";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3361355/ublock_origin-1.21.2-an+fx.xpi?src=";
-      sha256 = "6c7ecdb7704963b83e03f9ada79d70a9af7486c1149f41edac3ed760fec0ed7a";
-      meta = with stdenv.lib;
-      {
-        homepage = "https://github.com/gorhill/uBlock#ublock-origin";
-        description = "Finally, an efficient blocker. Easy on CPU and memory.";
-        license = licenses.gpl3;
+        homepage = "https://github.com/ueokande/vim-vixen";
+        description = "Accelerates your web browsing with Vim power!!";
+        license = licenses.mit;
         platforms = platforms.all;
         };
       };
